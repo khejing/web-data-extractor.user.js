71b4cc4bffbab4cad7ec46a9b5f3a0aa96798247
Will Lee <khejing@users.noreply.github.com>
2017-11-19 16:24:57 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Web Data Extractor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A universal web data extractor
+// @author       Will Lee
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
